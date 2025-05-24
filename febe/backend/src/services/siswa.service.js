94b48a4e6fe1096ae6d3ff2357a561f0916bbed5
Pafaz <paundra.afif@gmail.com>
2025-05-24 16:11:14 +0700

    update tabel hasil

--- febe/backend/src/services/siswa.service.js
+++ febe/backend/src/services/siswa.service.js
@@ -2,14 +2,38 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const { prediksiKelayakan } = require('../utils/aiRekomendasi');
 
-async function inputSiswa(userId, { namaSiswa, nomorIdentitas, tanggal }) {
-    const status = prediksiKelayakan(namaSiswa, nomorIdentitas);
+async function inputSiswa(
+    userId,
+    { 
+        namaSiswa, 
+        jenisKelamin, 
+        agama, 
+        alatTransportasi, 
+        pekerjaanOrtu, 
+        penghasilan, 
+        tanggungan, 
+        statusKIP, 
+        statusPKH 
+    }) {
+    const status = prediksiKelayakan(
+        alatTransportasi, 
+        pekerjaanOrtu, 
+        penghasilan, 
+        tanggungan, 
+        statusKIP, 
+        statusPKH);
 
     const hasil = await prisma.hasil.create({
         data: {
             namaSiswa,
-            nomorIdentitas,
-            tanggal: new Date(tanggal),
+            jenisKelamin,
+            Agama : agama,
+            alatTransportasi,
+            pekerjaanOrtu, 
+            penghasilan, 
+            tanggungan, 
+            statusKIP, 
+            statusPKH,
             status,
             userId
         }
